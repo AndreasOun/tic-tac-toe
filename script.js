025c491337019b6474f8358d081f920672c2f1ea
AndreasOun <andreastthk@gmail.com>
2023-04-30 21:37:55 +0300

    Added pop up

--- script.js
+++ script.js
@@ -1,147 +1,145 @@
 const gameBoard = (() => {
-    let board = ["", "", "", "", "", "", "", "", ""];
-    let currentPlayer = 'X';
-  
-    const getBoard = () => board;
-  
-    const getCurrentPlayer = () => currentPlayer;
-  
-    const makeMove = (index) => {
-      if (board[index] === '') {
-        board[index] = currentPlayer;
-        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-        return true;
-      }
-      return false;
-    };
-  
-    const reset = () => {
-      board = ["", "", "", "", "", "", "", "", ""];
-      currentPlayer = 'X';
-    };
-  
-    return { getBoard, getCurrentPlayer, makeMove, reset };
-  })();
-  
-  const Player = (name, marker) => {
-    const getName = () => name;
-    const getMarker = () => marker;
-  
-    return {
-      getName,
-      getMarker,
-    };
-  };
-  
-const player1 = Player("Player 1", "X");
-const player2 = Player("Player 2", "O");
-const gameController = (() => {
-    const checkWin = (board, player) => {
-      // Check rows
-      for (let i = 0; i <= 6; i += 3) {
-        if (board[i] === player && board[i + 1] === player && board[i + 2] === player) {
-          return true;
-        }
-      }
-    
-      // Check columns
-      for (let i = 0; i <= 2; i++) {
-        if (board[i] === player && board[i + 3] === player && board[i + 6] === player) {
-          return true;
-        }
-      }
-    
-      // Check diagonals
-      if (board[0] === player && board[4] === player && board[8] === player) {
-        return true;
-      }
-      if (board[2] === player && board[4] === player && board[6] === player) {
-        return true;
-      }
-    
-      return false;
-    };
-    
-    const checkTie = (board) => {
-      return !board.includes('');
-    };
-  
-    const resetButton = document.querySelector('.reset');
-    resetButton.addEventListener('click', () => {
-      gameBoard.reset();
-      render();
-    });
-  
-    const cells = document.querySelectorAll('.cell');
-  
-    const makeMoveHandler = (index) => {
-      if (gameBoard.makeMove(index)) {
-        render();
-      }
-    };
-  
-    cells.forEach((cell, index) => {
-      cell.addEventListener('click', () => makeMoveHandler(index));
-    });
-  
-    const render = () => {
-  const board = gameBoard.getBoard();
-  const currentPlayer = gameBoard.getCurrentPlayer();
-  const currentPlayerDisplay = document.querySelector('.current-player');
+  let board = ["", "", "", "", "", "", "", "", ""];
+  let currentPlayer = 'X';
 
-  cells.forEach((cell, index) => {
-    cell.textContent = board[index];
-    cell.removeEventListener('click', makeMoveHandler);
+  const getBoard = () => board;
+
+  const getCurrentPlayer = () => currentPlayer;
+
+  const makeMove = (index) => {
     if (board[index] === '') {
-      const makeMoveHandler = () => {
-        if (gameBoard.makeMove(index)) {
-          render();
-        }
-      };
-      cell.addEventListener('click', makeMoveHandler);
-    }
-  });
-
-  if (currentPlayerDisplay) {
-    currentPlayerDisplay.textContent = `Current player: ${currentPlayer}`;
-  }
-
-  if (checkWin(board, currentPlayer)) {
-    console.log(`${currentPlayer} wins!`);
-  } else if (checkTie(board)) {
-    console.log('Tie game!');
-  }
-};
-  
-    return { render };
-  })();
-
-const checkWin = (board, player) => {
-    // Check rows
-    for (let i = 0; i <= 6; i += 3) {
-      if (board[i] === player && board[i + 1] === player && board[i + 2] === player) {
-        return true;
-      }
-    }
-  
-    // Check columns
-    for (let i = 0; i <= 2; i++) {
-      if (board[i] === player && board[i + 3] === player && board[i + 6] === player) {
-        return true;
-      }
-    }
-  
-    // Check diagonals
-    if (board[0] === player && board[4] === player && board[8] === player) {
+      board[index] = currentPlayer;
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
       return true;
     }
-    if (board[2] === player && board[4] === player && board[6] === player) {
-      return true;
-    }
-  
     return false;
   };
-  
+
+  const reset = () => {
+    board = ["", "", "", "", "", "", "", "", ""];
+    currentPlayer = 'X';
+  };
+
+  return { getBoard, getCurrentPlayer, makeMove, reset };
+})();
+
+const Player = (name, marker) => {
+  const getName = () => name;
+  const getMarker = () => marker;
+
+  return {
+    getName,
+    getMarker,
+  };
+};
+
+const player1 = Player("Player 1", "X");
+const player2 = Player("Player 2", "O");
+
+const gameController = (() => {
+  const checkWin = (board, player) => {
+    // Define all possible winning combinations
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    // Check if any winning combination is satisfied
+    for (let combo of winningCombos) {
+      if (board[combo[0]] === player &&
+          board[combo[1]] === player &&
+          board[combo[2]] === player) {
+        // Player has won
+        return true;
+      }
+    }
+
+    // Check if the board is full
+    if (board.filter(square => square === '').length === 0) {
+      // It's a tie
+      return true;
+    }
+
+    // No winner yet
+    return false;
+  };
+
   const checkTie = (board) => {
     return !board.includes('');
   };
+  
+  const winnerText = document.querySelector('#winner-text');
+  const resetButton = document.querySelector('.reset');
+  resetButton.addEventListener('click', () => {
+    gameBoard.reset();
+    render();
+  });
+
+  const cells = document.querySelectorAll('.cell');
+
+  const makeMoveHandler = (index) => {
+    if (gameBoard.makeMove(index)) {
+      render();
+    }
+  };
+
+  cells.forEach((cell, index) => {
+    cell.addEventListener('click', () => makeMoveHandler(index));
+  });
+
+  const render = () => {
+    const board = gameBoard.getBoard();
+    const currentPlayer = gameBoard.getCurrentPlayer();
+    const currentPlayerDisplay = document.querySelector('.current-player');
+  
+    cells.forEach((cell, index) => {
+      cell.textContent = board[index];
+    });
+  
+    winnerText.style.display = 'none';
+
+
+    if (checkWin(board, 'X')) {
+      showWinnerText(player1);
+      gameBoard.reset();
+      return;
+    }
+  
+    if (checkWin(board, 'O')) {
+      showWinnerText(player2);
+      gameBoard.reset();
+      return;
+    }
+  
+    if (checkTie(board)) {
+      showWinnerText('tie');
+      gameBoard.reset();
+      return;
+    }
+  
+    currentPlayerDisplay.textContent = currentPlayer;
+  };
+
+
+  const showWinnerText = (player) => {
+    const winnerText = document.getElementById('winner-text');
+    const winnerName = document.getElementById('winner-name');
+
+if (player === 'tie') {
+  winnerName.textContent = 'Tie game!';
+} else {
+  winnerName.textContent = `${player.getName()} wins!`;
+}
+
+setTimeout(() => {
+  winnerText.style.display = 'block';
+}, 1000);};
+
+render();
+})();
